e879ad5dc7582a560a1e86f9828d20dd27a56cce
iamkarthikkasula99 <iamkarthikkasula99@gmail.com>
2024-04-05 11:11:51 -0500

    readme added

--- controllers/cap.js
+++ controllers/cap.js
@@ -1,53 +1,80 @@
 var cap = require('../models/cap');
 // List of all cap
-exports.cap_list = function(req, res) {
- res.send('NOT IMPLEMENTED: cap list');
+exports.cap_list = function (req, res) {
+    res.send('NOT IMPLEMENTED: cap list');
 };
 // for a specific cap.
-exports.cap_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: cap detail: ' + req.params.id);
+exports.cap_detail = function (req, res) {
+    res.send('NOT IMPLEMENTED: cap detail: ' + req.params.id);
 };
 // Handle cap create on POST.
-exports.cap_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: cap create POST');
+exports.cap_create_post = function (req, res) {
+    res.send('NOT IMPLEMENTED: cap create POST');
 };
 // Handle cap delete from on DELETE.
-exports.cap_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: cap delete DELETE ' + req.params.id);
+exports.cap_delete = function (req, res) {
+    res.send('NOT IMPLEMENTED: cap delete DELETE ' + req.params.id);
 };
- 
+
 // Handle cap update form on PUT.
-exports.cap_update_put = function(req, res) {
-    res.send('NOT IMPLEMENTED: cap update PUT' + req.params.id);
+exports.cap_update_put = async function(req, res) {
+ console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+ try {
+ let toUpdate = await cap.findById( req.params.id)
+ // Do updates of properties
+ if(req.body.cap_type)
+ toUpdate.cap_type = req.body.cap_type;
+ if(req.body.size) toUpdate.size = req.body.size;
+ if(req.body.cost) toUpdate.cost = req.body.cost;
+ let result = await toUpdate.save();
+ console.log("Sucess " + result)
+ res.send(result)
+ } catch (err) {
+ res.status(500)
+ res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+ }
+};
+
+
+exports.cap_list = async function (req, res) {
+    try {
+        thecap = await cap.find();
+        res.send(thecap);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// for a specific cap.
+exports.cap_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await cap.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
    };
- 
- 
-exports.cap_list = async function(req, res) {
-    try{
-    thecap = await cap.find();
-    res.send(thecap);
+
+exports.cap_view_all_Page = async function (req, res) {
+    try {
+        thecaps = await cap.find();
+        res.render('cap', { title: 'cap Search Results', results: thecaps });
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-   };
- 
- 
-exports.cap_view_all_Page = async function(req, res) {
-    try{
-    thecaps = await cap.find();
-    res.render('cap', { title: 'cap Search Results', results: thecaps });
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-    };
- 
- 
+};
+
+
 // Handle cap create on POST.
-exports.cap_create_post = async function(req, res) {
+exports.cap_create_post = async function (req, res) {
     console.log(req.body)
     let document = new cap();
     // We are caping for a body, since POST does not have query parameters.
@@ -57,34 +84,12 @@ exports.cap_create_post = async function(req, res) {
     document.cap_type = req.body.cap_type;
     document.cap_size = req.body.cap_size;
     document.cap_cost = req.body.cap_cost;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-    };
-   
-   // Handle cap create on POST.
-exports.cap_create_post = async function(req, res) {
-    console.log(req.body)
-    let document = new cap();
-    // We are caping for a body, since POST does not have query parameters.
-    // Even though bodies can be in many different formats, we will be picky
-    // and require that it be a json object
-    // {"cap_type":"goat", "cost":12, "size":"large"}
-    document.cap_type = req.body.cap_type;
-    document.cap_size = req.body.cap_size;
-    document.cap_cost = req.body.cap_cost;
-    try{
-    let result = await document.save();
-    res.send(result);
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-    };
-   
+};
